47f93b1ded58ad18ca6ecc3e34f5da57f9b90b85
rrabit42 <gegiraffe@naver.com>
2020-03-02 01:20:50 +0900

    [tutorial]understanding router url

--- api/urls.py
+++ api/urls.py
@@ -7,8 +7,8 @@ from tutorial import views
 # Because we're using viewsets instead of views, we can automatically generate the URL conf for our API, by simply registering the viewsets with a router class.
 # Again, if we need more control over the API URLs we can simply drop down to using regular class-based views, and writing the URL conf explicitly.
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+router.register(r'users', views.UserViewSet)    # http://127.0.0.1:8000/users/ 가능, 밑에 include(router.urls)때문
+router.register(r'groups', views.GroupViewSet)  # http://127.0.0.1:8000/groups/ 가능, 밑에 include(router.urls)때문
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
