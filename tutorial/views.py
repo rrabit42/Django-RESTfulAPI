07e2b21631d92ca0bd0ebea8ba446dec9f7cc67c
rrabit42 <gegiraffe@naver.com>
2020-03-02 01:06:43 +0900

    [tutorial]create views

--- tutorial/views.py
+++ tutorial/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from .serializers import UserSerializer, GroupSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    # API endpoint that allows users to be views or edited.
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    # API endpoint that allows groups to be viewed or edited.
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    
+"""
+Rather than write multiple views we're grouping together all the common behavior into classes called ViewSets.
+We can easily break these down into individual views if we need to, but using viewsets keeps the view logic nicely organized as well as being very concise.
+"""
