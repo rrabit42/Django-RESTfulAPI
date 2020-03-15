cd26c971a99389e15a1b872abff818b5f6fe20dc
rrabit42 <gegiraffe@naver.com>
2020-03-16 00:47:27 +0900

    [tutorial] add View & path

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
+from snippets import views
 
+urlpatterns = [
+    path('snippets/', views.snippet_list),
+    path('snippets/<int:pk>/', views.snippet_detail),
 ]
