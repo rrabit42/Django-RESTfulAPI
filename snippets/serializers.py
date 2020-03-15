ee244fddc8afd2a89ad4dbdc2376243d92e04122
rrabit42 <gegiraffe@naver.com>
2020-03-16 01:13:03 +0900

    [tutorial] change view - request.data, Response

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,9 +1,8 @@
 from rest_framework import serializers
-
 from snippets.models import LANGUAGE_CHOICES, STYLE_CHOICES, Snippet
 
 
-class SnippetSerializer(serializers.Serializer):
-   class Meta:
-       model = Snippet
-       fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
