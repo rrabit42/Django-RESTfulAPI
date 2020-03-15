bbabb537f0f7179bb9095112667af59bc96e6af3
rrabit42 <gegiraffe@naver.com>
2020-03-16 02:22:11 +0900

    [tutorial] using generic class based view

--- snippets/views.py
+++ snippets/views.py
@@ -12,33 +12,11 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 
-# 기본 뷰(GenericAPIView)는 핵심 기능을 제공하며, 믹스인 클래스들은 .list()나 .create() 기능을 제공
-# 여기서는 이 기능들을 get과 post 메서드에 적절히 연결
-class SnippetList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
