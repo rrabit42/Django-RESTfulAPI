2f0e521d60dcdcf4a77faab3c4b812f348bbbfe5
rrabit42 <gegiraffe@naver.com>
2020-03-16 11:47:24 +0900

    [tutorial] connect user - snippets 지금까지는 코드 조각을 만들었더라도, 해당 코드 조각을 만든 사용자와 아무런 관계도 맺지 않았음. 사용자는 직렬화된 표현에 나타나지 않았고, 요청하는 측에서 지정하는 속성이었을 뿐. 이를 해결함

--- snippets/views.py
+++ snippets/views.py
@@ -9,6 +9,9 @@ class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    def perform_create(self, serializer): # 이 메서드는 인스턴스를 저장하는 과정을 조정, 라서 요청이나 요청 URL에서 정보를 가져와 원하는 대로 다룰 수 있음.
+        serializer.save(owner=self.request.user) # 우리가 만든 시리얼라이저의 create() 메서드는 검증한 요청 데이터에 더하여 'owner' 필드도 전달.
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
