99662af0fdf7fa71c67a22c44504acb3afb473b1
rrabit42 <gegiraffe@naver.com>
2020-03-17 02:22:25 +0900

    [tutorial] refactoring to use ViewSets

--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
-from rest_framework import status, mixins, generics, permissions, renderers
-from rest_framework.decorators import api_view
+from rest_framework import status, mixins, generics, permissions, renderers, viewsets
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
@@ -9,28 +9,32 @@ from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
 
 
-class SnippetList(generics.ListCreateAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,) # REST 프레임워크는 특정 뷰에 제한을 걸 수 있는 권한 클래스를 제공하고 있음. 그 중 한가지인 IsAUthenticatedOrReadOnly는 인증 받은 요청에 읽기, 쓰기 권한 부여, 인증 받지 않은 요청은 읽기 권한만 부여
+class SnippetViewSet(viewsets.ModelViewSet): # 읽기 기능과 쓰기 기능을 모두 지원하기 위해 ModelViewSet 사용
+    """
+    이 뷰셋은 `list`와 `create`, `retrieve`, `update`, `destroy` 기능을 자동으로 지원합니다.
 
-    def perform_create(self, serializer): # 이 메서드는 인스턴스를 저장하는 과정을 조정, 라서 요청이나 요청 URL에서 정보를 가져와 원하는 대로 다룰 수 있음.
-        serializer.save(owner=self.request.user) # 우리가 만든 시리얼라이저의 create() 메서드는 검증한 요청 데이터에 더하여 'owner' 필드도 전달.
-
-
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+        여기에 `highlight` 기능의 코드만 추가로 작성했습니다.
+    """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly,)
 
+    # This decorator can be used to add any custom endpoints that don't fit into the standard create/update/delete style.
+    # Custom actions which use the @action decorator will respond to GET requests by default. We can use the methods argument if we wanted an action that responded to POST requests.
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
-class UserDetail(generics.RetrieveAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet): # ReadOnlyModelViewSet은 '읽기 전용' 기능을 자동 지원
+    """
+    이 뷰셋은 `list`와 `detail` 기능을 자동으로 지원합니다
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
@@ -44,13 +48,3 @@ def api_root(request, format=None):
     })
 
 
-class SnippetHighlight(generics.GenericAPIView):
-    # Rest 프레임워크에서 HTML로 렌더링하는 방식은 두가지가 있음
-    # 1. 템플릿 사용
-    # 2. 미리 렌더링된 HTML 사용(우리가 할 것)
-    queryset = Snippet.objects.all()
-    renderer_classes = (renderers.StaticHTMLRenderer,)
-
-    def get(self, request, *args, **kwargs):
-        snippet = self.get_object()
-        return Response(snippet.highlighted)
