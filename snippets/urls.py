c686c6834972497fe343e6fe9771b1eda046c9c4
rrabit42 <gegiraffe@naver.com>
2020-03-17 16:33:48 +0900

    [tutorial] Binding ViewSets to URLs explicitly

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,12 +1,35 @@
 from django.urls import path
 from snippets import views
+from snippets.views import SnippetViewSet, UserViewSet, api_root
+from rest_framework import renderers
+
+# ViewSet 클래스의 view들을 Http 메서드에 따라 어떻게 실제 뷰와 연결했는지
+snippet_list = SnippetViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+snippet_detail = SnippetViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+snippet_highlight = SnippetViewSet.as_view({
+    'get': 'highlight' # HTTP 메서드 : 내가 만든 뷰(실제 뷰, concrete view)
+}, renderer_classes=[renderers.StaticHTMLRenderer])
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
 
 
 urlpatterns = [
-    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
-    path('snippets/<int:pk>', views.SnippetDetail.as_view(), name='snippet-detail'),
-    path('users/', views.UserList.as_view(), name='user-list'),
-    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+    path('snippets/', snippet_list, name='snippet-list'),
+    path('snippets/<int:pk>', snippet_detail, name='snippet-detail'),
+    path('users/', user_list, name='user-list'),
+    path('users/<int:pk>/', user_detail, name='user-detail'),
     path('', views.api_root),
-    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
+    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
 ]
