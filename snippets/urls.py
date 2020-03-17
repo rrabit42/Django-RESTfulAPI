9a4f584f57f27b9e1991192454b302c53f377395
rrabit42 <gegiraffe@naver.com>
2020-03-17 16:40:58 +0900

    [tutorial] Using Routers

--- snippets/urls.py
+++ snippets/urls.py
@@ -3,33 +3,18 @@ from snippets import views
 from snippets.views import SnippetViewSet, UserViewSet, api_root
 from rest_framework import renderers
 
-# ViewSet 클래스의 view들을 Http 메서드에 따라 어떻게 실제 뷰와 연결했는지
-snippet_list = SnippetViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-snippet_detail = SnippetViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-snippet_highlight = SnippetViewSet.as_view({
-    'get': 'highlight' # HTTP 메서드 : 내가 만든 뷰(실제 뷰, concrete view)
-}, renderer_classes=[renderers.StaticHTMLRenderer])
-user_list = UserViewSet.as_view({
-    'get': 'list'
-})
-user_detail = UserViewSet.as_view({
-    'get': 'retrieve'
-})
+# View 클래스 대신 ViewSet 클래스를 사용했기 때문에, 이제는 URL도 설정할 필요가 없습니다.
+# Router 클래스를 사용하면 뷰 코드와 뷰, URL이 관례적으로 자동 연결됩니다.
+# 단지 뷰를 라우터에 적절히 등록해주기만 하면 됩니다. 그러면 REST 프레임워크가 알아서 다 합니다.
 
+#라우터를 생성하고 뷰셋을 등록합니다
+router = DefaultRouter() # DefaultRouter 클래스는 API의 최상단 뷰를 자동으로 생성해주므로, views 모듈에 있는 api_root 메서드와 연결했던 URL도 삭제하였습니다.
+router.register('snippets', views.SnippetViewSet) # 뷰들에 사용할 url 접두어와 뷰셋 등록
+router.register('users', views.User)
 
+# 이제 API URL을 라우터가 자동으로 인식합니다
+# 추가로 탐색 가능한 API를 구현하기 위해 로그인에 사용할 URL은 직접 설정을 했습니다.
 urlpatterns = [
-    path('snippets/', snippet_list, name='snippet-list'),
-    path('snippets/<int:pk>', snippet_detail, name='snippet-detail'),
-    path('users/', user_list, name='user-list'),
-    path('users/<int:pk>/', user_detail, name='user-detail'),
-    path('', views.api_root),
-    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
+    path('', include(router.urls)),
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework'))
 ]
